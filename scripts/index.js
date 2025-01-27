e4e771c2bf113df8696d4b38cfd1ca0ccaefbfe7
Vivian <vivian.bonilla@outlook.com>
2025-01-27 09:08:24 -0700

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
 function toggle() {
-    let main = document.querySelector("main")
+    let main = document.querySelector("main") ; 
+    let sidebar = document.querySelector(".sidebar");
+    let a = document.querySelectorAll("a");
+    let card = document.querySelectorAll(".card");
     main.classList.toggle("lightmode");
+    sidebar.classList.toggle("lightmode2");
+
+    for (let i = 0; i < card.length; i++) {
+        card[i].classList.toggle("lightmode2")
+    };
+
+    for (let i = 0; i < a.length; i++) {
+        a[i].classList.toggle("lightmode2")
+    };
 }
